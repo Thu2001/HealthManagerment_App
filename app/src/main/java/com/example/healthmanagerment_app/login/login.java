4e08e39b1cd1c083207400c5b8e62a0c9bda3bf5
tinddt <tinddt@upsharework.com>
2023-05-23 20:41:39 +0700

    navigation

---
+++ app/src/main/java/com/example/healthmanagerment_app/login/login.java
@@ -0,0 +1,47 @@
+package com.example.healthmanagerment_app.login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.healthmanagerment_app.R;
+
+public class login extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        Button login = (Button) view.findViewById(R.id.button123);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_login2_to_mainScreen);
+            }
+        });
+        TextView register = (TextView) view.findViewById(R.id.register_user);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_login2_to_register);
+            }
+        });
+
+        return view;
+    }
+}
