43c9beadc4b71c463e1a84dfb336606d61777ee9
tinddt <tinddt@upsharework.com>
2023-05-26 18:13:27 +0700

    api login

--- app/src/main/java/com/example/healthmanagerment_app/login/login.java
+++ app/src/main/java/com/example/healthmanagerment_app/login/login.java
@@ -15,6 +15,13 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.healthmanagerment_app.R;
+import com.example.healthmanagerment_app.api.API;
+import com.example.healthmanagerment_app.api.RetrofitClient;
+import com.example.healthmanagerment_app.model.data;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class login extends Fragment {
     @Override
@@ -31,7 +38,7 @@ public class login extends Fragment {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_login2_to_mainScreen);
+               Login("1","1",view);
             }
         });
         TextView register = (TextView) view.findViewById(R.id.register_user);
@@ -43,5 +50,25 @@ public class login extends Fragment {
         });
 
         return view;
+    }
+    public Boolean Login(String Acc,String Pas, View view){
+        Boolean check =false;
+        API methods = RetrofitClient.getRetrofit().create(API.class);
+        Call<data> call = methods.getdata();
+        call.enqueue(new Callback<data>() {
+            @Override
+            public void onResponse(Call<data> call, Response<data> response) {
+                data dt = response.body();
+                if(dt.data.get(1)!=null){
+                    Navigation.findNavController(view).navigate(R.id.action_login2_to_mainScreen);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<data> call, Throwable t) {
+
+            }
+        });
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/healthmanagerment_app/model/data.java
@@ -0,0 +1,9 @@
+package com.example.healthmanagerment_app.model;
+
+import java.util.ArrayList;
+
+public class data {
+    public String message;
+    public ArrayList<user> data;
+}
+
