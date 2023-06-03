10cdd614aef6d746ce95148f8086c6e41f251951
thuan <Thuangl2001@gmail.com>
2023-06-03 15:24:34 +0700

    test QR 1

--- app/src/main/java/com/example/healthmanagerment_app/mainScreen/info/info.java
+++ app/src/main/java/com/example/healthmanagerment_app/mainScreen/info/info.java
@@ -2,8 +2,10 @@ package com.example.healthmanagerment_app.mainScreen.info;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresPermission;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -11,7 +13,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
 
 import com.example.healthmanagerment_app.R;
 import com.example.healthmanagerment_app.api.API;
@@ -20,13 +24,20 @@ import com.example.healthmanagerment_app.mainScreen.info.uploadinfo.uploadinfo1;
 import com.example.healthmanagerment_app.model.User;
 import com.example.healthmanagerment_app.model.Data;
 import com.google.gson.Gson;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+
 public class info extends Fragment {
-    TextView name,birthday,age,address,phone,height,weight,userjob,benhly,tieusu;
+    TextView name,birthday,age,address,phone,height,weight,userjob,benhly,tieusu,text01;
+    ImageView qr_codeinfo;
     @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,7 +53,7 @@ public class info extends Fragment {
         weight = view.findViewById(R.id.weight);
         benhly = view.findViewById(R.id.benhly);
         tieusu = view.findViewById(R.id.tieusu);
-
+        qr_codeinfo= view.findViewById(R.id.qr_codeinfo);
         User user = new User();
         user.setAccount("1");
         Log.v("aaaaaaaaa",new Gson().toJson(user));
@@ -64,7 +75,19 @@ public class info extends Fragment {
                 weight.setText(dt.data.get(0).getUserWeight().toString());
                 benhly.setText(dt.data.get(0).getUserWeight().toString());
                 tieusu.setText(dt.data.get(0).getUserWeight().toString());
+                text01.setText("text01");
+
+                MultiFormatWriter multiFormatWriter=new MultiFormatWriter();
+                BitMatrix bitMatrix= null;
+                try {
+                    bitMatrix = multiFormatWriter.encode(text01.getText().toString(), BarcodeFormat.QR_CODE,150,150);
+                    BarcodeEncoder barcodeEncoder=new BarcodeEncoder();
+                    Bitmap bitmap=barcodeEncoder.createBitmap(bitMatrix);
+                } catch (WriterException e) {
+                    throw new RuntimeException(e);
+                }
             }
+
 
             @Override
             public void onFailure(Call<Data> call, Throwable t) {
@@ -72,6 +95,7 @@ public class info extends Fragment {
             }
 
         });
+
         Button uploadTT= view.findViewById(R.id.captt);
         uploadTT.setOnClickListener(new View.OnClickListener() {
             @Override
